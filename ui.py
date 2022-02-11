bb6e0449ec4d528b251b497d85cdbe1ace9aa281
Pliszka <karoliniakkjk@gmail.com>
2022-02-11 20:47:01 +0100

    tkinter object created (class QuizGUI)

--- ui.py
+++ ui.py
@@ -1,2 +1,10 @@
+from tkinter import Tk
 THEME_COLOR = "#375362"
 
+class QuizGUI(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title('QuizApp')
+
+        self.mainloop()
