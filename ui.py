b4d7c867e653b6624e5d43cdb314e5cd731c32f5
Pliszka <karoliniakkjk@gmail.com>
2022-02-11 21:38:38 +0100

    score label

--- ui.py
+++ ui.py
@@ -1,10 +1,15 @@
-from tkinter import Tk
+from tkinter import *
 THEME_COLOR = "#375362"
 
-class QuizGUI(Tk):
+class QuizGUI:
 
-    def __init__(self):
-        super().__init__()
-        self.title('QuizApp')
+    def __init__(self, current_score):
+        self.root = Tk()
+        self.root.title('QuizApp')
+        self.root.config(padx= 20, pady=20, bg=THEME_COLOR)
 
-        self.mainloop()
+        self.score_label = Label(text=f'Score: {current_score}', bg=THEME_COLOR, fg='white', font=('Arial', 10, 'italic'))
+        self.score_label.grid(row=0, column=1)
+
+        self.question_label = Label()
+        self.root.mainloop()
